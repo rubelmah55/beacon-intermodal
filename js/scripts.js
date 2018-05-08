66bc056268111d2c32d66738a51dd2ec39eb890f
sumanengbd <sumanengbd@gmail.com>
2018-05-08 12:59:27 +0600

    About Us Page Complete

--- js/scripts.js
+++ js/scripts.js
@@ -38,5 +38,21 @@
         delay: 10,
         time: 1000
     });
+
+    /*** Smooth scroll */
+	$(function() {
+		$('.smoothScroll').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top - 70
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
 	
 }(jQuery));
